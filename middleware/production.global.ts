2c0c94124248bf8fcec94f0ddd4337a35874a086
negativems <negativems1@gmail.com>
2023-04-23 13:06:06 +0200

    :reycle: adds maintenance page for production

---
+++ middleware/production.global.ts
@@ -0,0 +1,6 @@
+export default defineNuxtRouteMiddleware((to) => {
+   const isProduction = process.env.NODE_ENV === 'production' ?? true;
+   if (isProduction && to.path !== '/maintenance') {
+      return '/maintenance';
+   }
+});
