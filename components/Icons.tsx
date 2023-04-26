db4278b1aa12c2bed1b0566ea9f65a1db55cd9a2
negativems <negativems1@gmail.com>
2023-04-27 00:49:29 +0200

    :recycle: adds some icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,11 +1,11 @@
-export const GoogleIcon = () => (
+export const GoogleIcon = ({ color = 'black' }) => (
    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M17.9167 8.0392L17.8134 7.62359H9.2733V11.3759H14.3813C14.0963 12.521 13.4483 13.534 12.5409 14.253C11.6337 14.972 10.5194 15.3555 9.37661 15.342C7.8853 15.3293 6.45247 14.7406 5.35907 13.6915C4.82158 13.1435 4.39387 12.4911 4.10038 11.7718C3.80691 11.0526 3.65344 10.2805 3.64876 9.49977C3.66611 7.93174 4.26622 6.43073 5.32465 5.30809C6.4026 4.25168 7.83434 3.66857 9.31921 3.68127C10.5909 3.6914 11.8154 4.18118 12.7628 5.05872L15.2881 2.37508C13.6237 0.83894 11.4694 -0.00685917 9.23886 0.000170556C8.0118 -0.00736977 6.7954 0.235179 5.65898 0.713984C4.52258 1.19278 3.48845 1.89844 2.61569 2.79068C0.95552 4.57457 0.0195285 6.94891 0.000301786 9.42508C-0.0189135 11.9013 0.880117 14.2908 2.51238 16.102C3.41671 17.034 4.49155 17.7704 5.67407 18.2678C6.85658 18.7653 8.12302 19.014 9.39956 18.9994C10.5571 19.0083 11.7044 18.774 12.7719 18.3107C13.8393 17.8473 14.8046 17.1647 15.6095 16.3039C17.1889 14.5074 18.0432 12.1534 17.9971 9.72539C18.0095 9.16206 17.9826 8.59861 17.9167 8.0392Z" fill="white" />
    </svg>
 );
 
-export const GithubIcon = ({ className = '', color = 'black' }) => (
-   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class={className}>
+export const GithubIcon = ({ color = 'black' }) => (
+   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
       <path
          d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z"
          fill={color}
@@ -21,15 +21,15 @@ export const Logo = ({ className = '', color = 'white' }) => (
    </svg>
 );
 
-export const LogginIcon = ({ className = '' }) => (
-   <div class={className}>
-      <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-         <path d="M10.9208 13.5594C10.6643 13.8152 10.6643 14.2305 10.9208 14.487C11.0488 14.6149 11.2167 14.6792 11.3846 14.6792C11.5526 14.6792 11.7205 14.6149 11.8485 14.487L15.7236 10.5095L11.8485 6.53204C11.5926 6.27619 11.1773 6.27619 10.9208 6.53204C10.6643 6.78788 10.6643 7.2038 10.9208 7.45965L13.2937 9.83249H0.656024C0.293899 9.83249 0 10.1264 0 10.4885C0 10.8506 0.293899 11.1445 0.656024 11.1445H13.3344L10.9208 13.5594ZM19.6873 0H7.87885C7.15395 0 6.56615 0.587142 6.56615 1.31205V7.21627H7.88738V2.1124C7.88738 1.67417 8.24295 1.31861 8.68117 1.31861H18.864C19.3029 1.31861 19.6578 1.67417 19.6578 2.1124L19.6788 18.8883C19.6788 19.3265 19.3239 19.682 18.885 19.682H8.68183C8.2436 19.682 7.88804 19.3265 7.88804 18.8883V13.7555L6.5668 13.7575V19.6814C6.5668 20.4063 7.1546 20.9934 7.87951 20.9934H19.688C20.4129 20.9934 21 20.4063 21 19.6814V1.3127C20.9993 0.587798 20.4122 0.000656024 19.688 0.000656024L19.6873 0Z" fill="black" />
-      </svg>
-   </div>
+export const LogginIcon = ({ color = 'black' }) => (
+   <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+      <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+      <polyline points="10 17 15 12 10 7"></polyline>
+      <line x1="15" y1="12" x2="3" y2="12"></line>
+   </svg>
 );
 
-export const BuyMeACoffeIcon = () => (
+export const BuyMeACoffeIcon = ({ color = 'black' }) => (
    <svg xmlns="http://www.w3.org/2000/svg" width="31" height="45" fill="none">
       <path fill="#000" d="m27.47 10.26-.03-.018-.072-.02a.169.169 0 0 0 .101.038Zm.007-.022a.053.053 0 0 1-.01-.002v.007a.02.02 0 0 0 .01-.005Zm.408 3.155.052-.03.018-.01.018-.018a.295.295 0 0 0-.088.058Zm-.33-3.071-.05-.048-.035-.018a.14.14 0 0 0 .085.066Zm-12.628 30.56a.262.262 0 0 0-.103.078l.032-.02c.022-.02.052-.043.071-.058Zm7.34-1.434c0-.044-.023-.036-.018.125 0-.013.006-.026.008-.039l.01-.086Zm-.762 1.434a.262.262 0 0 0-.104.078l.032-.02c.022-.02.052-.043.072-.058Zm-11.74.347a.217.217 0 0 0-.108-.051l.087.043.02.008ZM8.607 40.12a.338.338 0 0 0-.042-.133c.017.043.03.087.04.131l.002.002Z" />
       <path fill="#FD0" d="M16.41 20.357c-1.597.678-3.407 1.446-5.752 1.446a10.97 10.97 0 0 1-2.904-.397l1.622 16.538c.057.691.375 1.336.889 1.806s1.188.731 1.887.73c0 0 2.3.119 3.067.119.826 0 3.304-.12 3.304-.12.699 0 1.372-.26 1.886-.73.514-.47.83-1.114.888-1.805l1.737-18.274c-.777-.262-1.56-.437-2.444-.437-1.528-.001-2.759.52-4.18 1.125v-.001Z" />
@@ -38,7 +38,7 @@ export const BuyMeACoffeIcon = () => (
    </svg>
 );
 
-export const FooterCurveIcon = () => (
+export const FooterCurveIcon = ({ color = 'black' }) => (
    <svg class="absolute bottom-0 left-1/2 -translate-x-1/2 w-[1920px] overflow-hidden" width="1920" height="100" fill="none">
       <path fill="#9DFF7A" d="M0 0h1920v100H0z" />
       <path fill="#77FF47" d="M968.5 59.088C588.405 59.51 0 0 0 0v100h1920V.5s-578.2 58.173-951.5 58.588Z" />
@@ -46,48 +46,58 @@ export const FooterCurveIcon = () => (
    </svg>
 );
 
-export const CrossIcon = ({ color = 'red' }) => {
-   return (
+export const CrossIcon = ({ color = 'red' }) => (
       <svg class="w-5 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
          <path fill={color} d="m18.8 16 5.5-5.5c.8-.8.8-2 0-2.8-.3-.4-.8-.7-1.3-.7s-1 .2-1.4.6L16 13.2l-5.5-5.5c-.8-.8-2.1-.8-2.8 0-.4.3-.7.8-.7 1.4s.2 1 .6 1.4l5.5 5.5-5.5 5.5c-.3.4-.6.9-.6 1.5 0 .5.2 1 .6 1.4.4.4.9.6 1.4.6.5 0 1-.2 1.4-.6l5.5-5.5 5.5 5.5c.8.8 2.1.8 2.8 0 .8-.8.8-2.1 0-2.8L18.8 16z" />
       </svg>
    );
-};
 
-export const CheckIcon = ({ color = 'green' }) => {
-   return (
+export const CheckIcon = ({ color = 'green' }) => (
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 17.837 17.837">
          <path fill={color} d="M16.145 2.571a.7.7 0 0 0-.99 0L6.92 10.804l-4.241-4.27a.698.698 0 0 0-.989 0L.204 8.019a.703.703 0 0 0 0 .99l6.217 6.258a.704.704 0 0 0 .99 0L17.63 5.047a.7.7 0 0 0 0-.994l-1.485-1.482z" />
       </svg>
    );
-};
 
-export const ListBulletIcon = ({ className = '' }) => {
-   return (
-      <svg xmlns="http://www.w3.org/2000/svg" class={'w-6 h-6' + (className ? ' ' + className : '')} fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor">
+export const ListBulletIcon = ({ className = '' }) => (
+      <svg xmlns="http://www.w3.org/2000/svg" class={'w-6 h-6' + (className ? ' ' + className : '')} fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
       </svg>
    );
-};
 
-export const Clock = () => {
-   return (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1} stroke="currentColor">
+export const Clock = () => (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
    );
-};
 
-export const NumberedSquare = ({ number = 0, className = '' }) => {
-   return (
-      <div class={'flex items-center justify-center w-6 h-6 rounded-full border-black border-[1px] p-2' + (className ? ' ' + className : '')}>
-         <span class="text-gray-500">{number}</span>
-      </div>
-   );
-};
+export const NumberedSquare = ({ number = 0, className = '' }) => (
+   <div class={'flex items-center justify-center w-6 h-6 rounded-full border-black border-[1px] p-2' + (className ? ' ' + className : '')}>
+      <span class="text-gray-500">{number}</span>
+   </div>
+);
 
-export const SearchIcon = ({ className = '' }) => {
-   return (
-      <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="none"><path fill="#9DA0B8" fill-rule="evenodd" d="M8.944 2.556a6.389 6.389 0 1 0 0 12.777 6.389 6.389 0 0 0 0-12.777ZM0 8.944a8.944 8.944 0 1 1 16.108 5.357l6.518 6.518a1.278 1.278 0 0 1-1.807 1.807L14.3 16.108A8.944 8.944 0 0 1 0 8.944Z" clip-rule="evenodd" /></svg>
-   );
-};
+export const SearchIcon = () => (
+   <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="none">
+      <path fill="#9DA0B8" fill-rule="evenodd" d="M8.944 2.556a6.389 6.389 0 1 0 0 12.777 6.389 6.389 0 0 0 0-12.777ZM0 8.944a8.944 8.944 0 1 1 16.108 5.357l6.518 6.518a1.278 1.278 0 0 1-1.807 1.807L14.3 16.108A8.944 8.944 0 0 1 0 8.944Z" clip-rule="evenodd" />
+   </svg>
+);
+
+export const BookmarkIcon = ({ color = "black" }) => (
+   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path fill={color} d="M19 2.01H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.998 5 19.815 5 19.01c0-.101.009-.191.024-.273.112-.575.583-.717.987-.727H20c.018 0 .031-.009.049-.01H21V4.01c0-1.103-.897-2-2-2zm0 14H5v-11c0-.806.55-.988 1-1h7v7l2-1 2 1v-7h2v12z" />
+   </svg>
+);
+
+export const StarIcon = () => (
+   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"></path>
+   </svg>
+);
+
+export const LogoutIcon = ({ color = 'black' }) => (
+   <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+      <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+      <polyline points="16 17 21 12 16 7"></polyline>
+      <line x1="21" y1="12" x2="9" y2="12"></line>
+   </svg>
+);
