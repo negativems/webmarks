09a1d6c78c3bf6076e31e7f348ccc16d788f1b73
negativems <negativems1@gmail.com>
2023-04-27 00:51:17 +0200

    :recycle: fix search component and adds drag&drop feature

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,78 +1,59 @@
 <script setup lang="ts">
-import { SearchIcon } from '~/components/Icons';
+const user = useSupabaseUser();
+const client = useSupabaseClient();
 
 definePageMeta({
    layout: 'dashboard',
    // middleware: 'authentication'
 });
 
-const client = useSupabaseClient();
-const user = useSupabaseUser();
+const search = ref("");
+const bookmarks = ref(await getBookmarks(user, client));
+const hasBookmarks = computed(() => bookmarks.value.length > 0);
 
-const getBookmarks = async () => {
-   const { data } = await client.from('bookmarks').select('id, title, description, url').eq('user_id', user.value?.id).limit(100);
+const dragEnter = (e: DragEvent) => {
+   e.preventDefault();
+   e.stopPropagation();
 
-   return (data || []) as Bookmark[];
+   // add tailwind class to the body
+   document.querySelector('.bookmarks')?.classList.add("bg-red-300");
+   document.querySelector('.dark-backround-modal')?.classList.remove("hidden");
 };
 
-interface Bookmark {
-   id: number;
-   title: string;
-   description: string;
-   url: string;
-}
+const dragLeave = (e: DragEvent) => {
+   e.preventDefault();
+   e.stopPropagation();
 
-const bookmarks = ref(await getBookmarks());
-
-async function updateBookmarks() {
-   const { data } = await client.from('bookmarks').select('id, title, description, url').eq('user_id', user.value?.id).limit(100);
-   bookmarks.value = data || [];
-}
-
-async function addBookmark() {
-   const userId = user.value?.id;
-   const websites = bookmarks.value.map((bookmark: any) => ({ ...bookmark, user_id: userId }));
-   const { data } = await client.from('bookmarks').insert(websites as any);
-
-   return data;
-}
-
-async function deleteBookmark(id: number) {
-   const res = await client.from('bookmarks').delete().eq('id', id);
-   updateBookmarks();
-   return res;
-}
-
-const searchComponent = ref(null);
-let search = ref('');
-
-// TODO: Fix this
-
+   document.querySelector('.bookmarks')?.classList.remove("bg-red-300");
+   document.querySelector('.dark-backround-modal')?.classList.add("hidden");
+};
 </script>
 
 <template>
-   <div class="bookmarks flex flex-col gap-10">
-      <DashboardSearchBar ref="searchComponent" />
+   <div
+      class="bookmarks flex"
+      :class="{ 'flex-col gap-10': hasBookmarks, 'justify-center items-center h-full': !hasBookmarks }"
+      v-on:dragenter="dragEnter"
+      v-on:dragend="dragLeave"
+   >
+      <DashboardSearchBar
+         v-model:search="search"
+         v-if="bookmarks.length > 0"
+      />
 
-      <div class="bookmarks grid grid-cols-5 gap-5 mb-5">
-         <div class="bookmark inline-flex" v-for="bookmark in bookmarks.filter((bookmark: any) => bookmark.title.toLowerCase().includes(search.toLowerCase()))" :key="bookmark.id">
-            <div class="grid grid-cols-2 bg-white rounded-xl p-5 gap-5 w-[300px]">
-               <div class="col-span-2">
-                  <h3 class="text-xl font-bold">{{ bookmark.title }}</h3>
-                  <p class="text-gray-500 break-words">{{ bookmark.description }}</p>
-               </div>
-               <a :href="bookmark.url" class="bg-blue-200 text-blue-500 p-3 px-5 hover:bg-accent font-bold rounded-xl flex items-center justify-center max-h-[50px] self-end">
-                  <span>Go</span>
-               </a>
-               <button class="bg-red-300 text-white font-bold p-3 px-5 rounded-lg gap-5 hover:bg-gray-900 flex items-center justify-center  self-end" @click="deleteBookmark(bookmark.id)">
-                  <span>Delete</span>
-               </button>
-            </div>
-         </div>
+      <DashboardBookmarks :search="search" />
+
+      <div class="no-bookmarks border-8 border-dashed inline-flex flex-col items-center gap-5 rounded-2xl p-10 text-center backdrop-blur-3xl">
+         <h2 class="text-2xl font-bold">No bookmarks found</h2>
+         <p class="text-gray-500">
+            You don't have any bookmarks yet.
+            <br>
+            Create your first bookmark by clicking the button below or drag and drop here your bookmarks.
+         </p>
+         <button class="bg-accent text-white font-bold p-3 px-5 rounded-lg gap-5 hover:bg-gray-900 flex items-center justify-center mt-5">
+            <span>Create Bookmark</span>
+         </button>
       </div>
+      <div class="dark-backround-modal bg-black opacity-30 absolute left-0 top-0 w-screen h-screen hidden"></div>
    </div>
-
-   <button class="login-button w-full bg-red-400 text-white font-bold p-5 rounded-lg flex justify-center gap-5 hover:bg-gray-900 justify-self-end" @click="addBookmark()">
-      Add bookmark
-   </button>
 </template>
