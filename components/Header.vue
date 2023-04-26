1acb18f2aabe5aac87566421f37f73e9f75a57d3
negativems <negativems1@gmail.com>
2023-04-27 00:49:15 +0200

    :recycle: put script on top of the page

--- components/Header.vue
+++ components/Header.vue
@@ -1,34 +1,3 @@
-<template>
-   <header :class="'flex items-center bg-transparent mt-5' + (isHome ? ' absolute w-full' : '')">
-      <div :class="'container mx-auto flex justify-between items-center h-full px-10 box-content bg-white rounded-3xl'">
-         <div class="logo-container flex-1 relative h-full">
-            <HeaderLogo />
-         </div>
-         <div class="navbar flex gap-12 flex-1">
-            <NuxtLink
-               v-for="link in headerLinks"
-               :key="link.href"
-               :to="link.href"
-               :class="'w-full text-center hover:bg-accent-light p-3 rounded-xl ' + (selected === link.href ? 'border-b-4 border-accent rounded-none bg-accent-light' : '')"
-            >
-               {{ link.label }}
-            </NuxtLink>
-         </div>
-         <div
-            class="flex-1 flex justify-end"
-            v-if="user"
-         >
-            <NuxtLink
-               to="/dashboard"
-               class="btn btn-primary border-2 border-accent bg-white hover:shadow-solid-xl duration-150 p-3 rounded-xl"
-            >
-               Dashboard
-            </NuxtLink>
-         </div>
-      </div>
-   </header>
-</template>
-
 <script setup lang="ts">
 const route = useRoute();
 const path = ref(route.path);
@@ -59,3 +28,34 @@ watchEffect(() => {
    isHome.value = path.value === '/';
 });
 </script>
+
+<template>
+   <header :class="'flex items-center bg-transparent mt-5' + (isHome ? ' absolute w-full' : '')">
+      <div :class="'container mx-auto flex justify-between items-center h-full px-10 box-content bg-white rounded-3xl'">
+         <div class="logo-container flex-1 relative h-full">
+            <HeaderLogo />
+         </div>
+         <div class="navbar flex gap-12 flex-1">
+            <NuxtLink
+               v-for="link in headerLinks"
+               :key="link.href"
+               :to="link.href"
+               :class="'w-full text-center hover:bg-accent-light p-3 rounded-xl ' + (selected === link.href ? 'border-b-4 border-accent rounded-none bg-accent-light' : '')"
+            >
+               {{ link.label }}
+            </NuxtLink>
+         </div>
+         <div
+            class="flex-1 flex justify-end"
+            v-if="user"
+         >
+            <NuxtLink
+               to="/dashboard"
+               class="btn btn-primary border-2 border-accent bg-white hover:shadow-solid-xl duration-150 p-3 rounded-xl"
+            >
+               Dashboard
+            </NuxtLink>
+         </div>
+      </div>
+   </header>
+</template>
