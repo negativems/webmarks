a469c644ea7f57027891499fc89d6e9f228bff88
negativems <negativems1@gmail.com>
2023-05-01 19:19:14 +0200

    ♻️ Adds ecosystem.config.js for deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+   apps: [
+      {
+         name: 'Webmarks',
+         port: '8030',
+         exec_mode: 'cluster',
+         instances: 'max',
+         script: './.output/server/index.mjs'
+      }
+   ]
+};
