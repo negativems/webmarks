57c077b80cc6d4a88edb29738cf204919e93b571
negativems <negativems1@gmail.com>
2023-05-01 19:12:05 +0200

    ♻️ Change some home hero component styles

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,14 +1,22 @@
 <template>
-   <div class="hero bg-white h-[700px] pt-[120px] bg-no-repeat bg-right-bottom bg-contain" style="background-image: url(&quot;/head-curve.svg&quot;);">
-      <div class="container mx-auto flex justify-between relative z-10">
-         <div class="left flex flex-col gap-20 flex-1">
-            <h1 class="drop-shadow-solid-md text-5xl font-extrabold">The simplest way to manage your bookmarks</h1>
+   <div class="hero flex h-[700px] bg-white bg-contain bg-right-bottom bg-no-repeat pt-[60px] dark:bg-gray-900">
+      <div class="container relative mx-auto flex">
+         <div class="hero-background-form absolute left-[100px] top-[155px] h-20 w-[220px] rounded-full bg-accent-light blur-[30px]"></div>
+         <div class="left flex flex-1 flex-col justify-center gap-40">
+            <h1 class="font-sans text-5xl font-extrabold drop-shadow-solid-md dark:text-white">
+               The simplest way to manage your bookmarks
+            </h1>
             <div class="buttons">
-               <NuxtLink to="/dashboard" class="bg-accent px-6 py-3 rounded-lg shadow-md hover:shadow-xl duration-150 hover:scale-110 inline-block font-bold active:bg-accent-dark focus:ring focus:bg-accent-light ring-accent-dark">Try it</NuxtLink>
+               <NuxtLink
+                  to="/dashboard"
+                  class="inline-block rounded-lg bg-accent px-6 py-3 font-bold shadow-solid-md ring-accent-dark duration-150 hover:scale-110 hover:shadow-solid-xl focus:bg-accent-light focus:ring active:bg-accent-dark"
+               >
+                  Try it
+               </NuxtLink>
             </div>
          </div>
-         <div class="right flex-1 flex justify-end">
-            <div class="square w-80 h-80 bg-gray-300 rounded-2xl shadow-solid-xl p-5">
+         <div class="right flex flex-1 items-center justify-end">
+            <div class="square h-2/3 w-80 rounded-2xl bg-gray-300 p-5 shadow-solid-xl">
                asd
             </div>
          </div>
