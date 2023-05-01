506cecbacb8c2a35208a7fc8d67b2bc53e47ba58
negativems <negativems1@gmail.com>
2023-05-01 19:13:19 +0200

    ♻️ Adds dark mode to plan card component

--- components/home/PlanCard.vue
+++ components/home/PlanCard.vue
@@ -1,21 +1,27 @@
 <template>
-   <div :class="'plan-card p-10 shadow-xl rounded-3xl relative ' + (dark ? 'bg-accent-darker text-white' : 'bg-white') + ' ' + className">
-      <header class="plan-card-header flex justify-between relative mb-10">
-         <h3 class="text-center w-full">{{ name }}</h3>
-         <span v-if="saveAmount" class="plan-card-save absolute top-1/2 -translate-y-1/2 right-0 px-3 py-1 rounded-md text-[10px]" :class="(dark ? 'bg-white text-black' : 'bg-red-400')">
+   <div
+      class="plan-card-container flex flex-col items-center justify-between rounded-2xl"
+      :class="{
+         'bg-white text-black': !dark,
+         'bg-accent-darker text-white': dark
+      }"
+   >
+      <header class="plan-card-header relative mb-10 flex justify-between">
+         <h3 class="w-full text-center">{{ name }}</h3>
+         <span v-if="saveAmount" class="plan-card-save absolute right-0 top-1/2 -translate-y-1/2 rounded-md px-3 py-1 text-[10px]" :class="(dark ? 'bg-white text-black' : 'bg-red-400')">
             Saves {{ saveAmount }}%
          </span>
       </header>
       <div class="plan-card-body">
          <ul class="flex flex-col gap-4">
-            <li v-for="(feature, index) in features" :key="index" class="flex flex-row gap-2 items-center">
+            <li v-for="(feature, index) in features" :key="index" class="flex flex-row items-center gap-2">
                <CheckIcon />
                <span>{{ feature }}</span>
             </li>
          </ul>
       </div>
       <footer class="plan-card-footer">
-         <div class="plan-card-price text-center font-extrabold text-2xl my-10">
+         <div class="plan-card-price my-10 text-center text-2xl font-extrabold">
             <span class="plan-card-price-currency">$</span>
             {{ price }}/month
          </div>
@@ -32,34 +38,13 @@ import { CheckIcon, CrossIcon } from '~/components/Icons';
 export default defineComponent({
    name: 'PlanCard',
    props: {
-      name: {
-         type: String,
-         required: true
-      },
-      features: {
-         type: Array,
-         required: true
-      },
-      price: {
-         type: Number,
-         required: true
-      },
-      tryable: {
-         type: Boolean,
-         default: false
-      },
-      saveAmount: {
-         type: Number,
-         default: null
-      },
-      dark: {
-         type: Boolean,
-         default: false
-      },
-      className: {
-         type: String,
-         default: ''
-      }
+      name: { type: String, required: true },
+      features: { type: Array, required: true },
+      price: { type: Number, required: true },
+      tryable: { type: Boolean, default: false },
+      saveAmount: { type: Number, default: null },
+      dark: { type: Boolean, default: false },
+      className: { type: String, default: '' }
    },
    methods: {
       tryForFree() {
