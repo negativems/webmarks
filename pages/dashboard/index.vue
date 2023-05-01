3cc13fcd270c124ca7460eb9c575919c3f9bb047
negativems <negativems1@gmail.com>
2023-05-01 19:16:58 +0200

    ♻️ Load bookmarks from the store

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-const user = useSupabaseUser();
-const client = useSupabaseClient();
+import { store } from '~/store/store';
+import { Bookmark } from '~/types/types';
 
 definePageMeta({
    layout: 'dashboard',
@@ -8,14 +8,21 @@ definePageMeta({
 });
 
 const search = ref("");
-const bookmarks = ref(await getBookmarks(user, client));
-const hasBookmarks = computed(() => bookmarks.value.length > 0);
+const bookmarks = store.bookmarks as Bookmark[];
+const hasBookmarks = computed(() => store.bookmarks.length > 0);
+
+onMounted(() => {
+   if (!isLogged()) {
+      store.bookmarks = localStorage.getItem('bookmarks') ? JSON.parse(localStorage.getItem('bookmarks')!) : [];
+      console.log('store', store.bookmarks);
+   }
+});
 </script>
 
 <template>
    <div
       class="bookmarks flex"
-      :class="{ 'flex-col gap-10': hasBookmarks, 'justify-center items-center h-full': !hasBookmarks }"
+      :class="{ 'flex-col gap-10': hasBookmarks, 'h-full items-center justify-center': !hasBookmarks }"
    >
       <DashboardSearchBar
          v-model:search="search"
@@ -25,7 +32,7 @@ const hasBookmarks = computed(() => bookmarks.value.length > 0);
       <DashboardBookmarks :search="search" />
 
       <div
-         class="no-bookmarks border-8 border-dashed inline-flex flex-col items-center gap-5 rounded-2xl p-10 text-center backdrop-blur-3xl"
+         class="no-bookmarks inline-flex flex-col items-center gap-5 rounded-2xl border-8 border-dashed p-10 text-center backdrop-blur-3xl"
          v-if="!hasBookmarks"
       >
          <h2 class="text-2xl font-bold">No bookmarks found</h2>
@@ -34,7 +41,7 @@ const hasBookmarks = computed(() => bookmarks.value.length > 0);
             <br>
             Create your first bookmark by clicking the button below or drag and drop here your bookmarks.
          </p>
-         <button class="bg-accent text-white font-bold p-3 px-5 rounded-lg gap-5 hover:bg-gray-900 flex items-center justify-center mt-5">
+         <button class="mt-5 flex items-center justify-center gap-5 rounded-lg bg-accent p-3 px-5 font-bold text-white hover:bg-gray-900">
             <span>Create Bookmark</span>
          </button>
       </div>
