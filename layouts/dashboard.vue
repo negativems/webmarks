09e4753e7288b642a1fd522feeea665f0e759032
negativems <negativems1@gmail.com>
2023-05-01 19:16:13 +0200

    ♻️ Adds dark mode initialization on the layouts

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import { store } from '~/store/store';
+const cookieTheme = useCookie('theme').value;
+
+if (cookieTheme && store.theme !== cookieTheme) {
+   store.setTheme(cookieTheme);
+}
+
+const isDark = computed(() => store.theme === 'dark');
+
+useHead({
+   title: 'Webmarks - Bookmark Manager',
+   bodyAttrs: {
+      class: isDark.value ? 'dark' : 'bg-gray-100',
+   },
+});
+//    const isDarkModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches;
+</script>
+
 <template>
-   <div class="dashboard bg-accent-light flex min-h-[300px] h-[100vh] max-h-[100vh]">
+   <div class="dashboard flex h-[100vh] max-h-[100vh] min-h-[300px] bg-accent-light">
       <DashboardSideBar />
       <DashboardContent>
          <slot />
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,30 @@
+<script setup lang="ts">
+import { store } from '~/store/store';
+const cookieTheme = useCookie('theme').value;
+
+if (cookieTheme && store.theme !== cookieTheme) {
+   store.setTheme(cookieTheme);
+}
+
+const isDark = computed(() => store.theme === 'dark');
+
+useHead({
+   title: 'Webmarks - Bookmark Manager',
+   bodyAttrs: {
+      class: isDark.value ? 'dark' : 'bg-gray-100',
+   },
+});
+//    const isDarkModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches;
+</script>
+
 <template>
    <Header />
    <NuxtPage />
    <Footer />
 </template>
+
+<style lang="scss">
+body.dark {
+   @apply bg-gray-800 text-white;
+}
+</style>
