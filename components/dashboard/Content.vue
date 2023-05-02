e9a067a1eb1fd4e9f0d39b5ac8f12f61d8d8dde4
negativems <negativems1@gmail.com>
2023-05-02 14:50:34 +0200

    ♻️ Change bookmark title to the domain of the website

--- components/dashboard/Content.vue
+++ components/dashboard/Content.vue
@@ -5,7 +5,6 @@ import { type Bookmark } from '~/types/types';
 const dragEnter = (e: DragEvent) => {
    e.preventDefault();
    e.stopPropagation();
-   // add tailwind class to the body
    document.querySelector('main')?.classList.add("bg-gray-300");
 };
 
@@ -24,9 +23,6 @@ const dragLeave = (e: any) => {
    document.querySelector('main')?.classList.remove("bg-gray-300");
 };
 
-const user = useSupabaseUser();
-const client = useSupabaseClient();
-
 const drop = async (e: any) => {
    e.preventDefault();
    e.stopPropagation();
@@ -35,22 +31,18 @@ const drop = async (e: any) => {
 
    document.querySelector('main')?.classList.remove("bg-gray-300");
 
-   // Check if the data transfer is not an url and it is a bookmark folder (not a folder or file)
    if (plainInput.startsWith("http")) {
       const url = e.dataTransfer.getData("text/plain");
-      const user_id: string | undefined = user.value?.id ?? undefined;
-      const title = new URL(url).hostname.split('.')[0];
-
-      const bookmark: Bookmark = { user_id, url, title };
-
-      store.addBookmark(bookmark, user.value, client);
+      const title = new URL(url).hostname;
+      const bookmark: Bookmark = { url, title };
+      store.addBookmark(bookmark);
    }
 };
 </script>
 
 <template>
    <main
-      className="bg-gray-100 flex-1 rounded-3xl p-10 m-5 h-min-[500px] overflow-auto"
+      className="dark:bg-gray-800 bg-gray-100 flex-1 rounded-3xl p-10 m-5 h-min-[500px] overflow-auto"
       @dragenter="dragEnter"
       @dragleave="dragLeave"
       @drop="drop"
