efda13337f7ad05b5cdca1491d4423c75c228d1a
negativems <negativems1@gmail.com>
2023-05-11 09:53:11 +0200

    :recycle: Test CI/CD

--- middleware/construction.global.ts
+++ middleware/construction.global.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
-   const isProduction = process.env.NODE_ENV === 'production' ?? true;
+   // const isProduction = process.env.NODE_ENV === 'production' ?? true;
+   const isProduction = false;
    if (isProduction && to.path !== '/construction') {
       return navigateTo('/construction');
    }
